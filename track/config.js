194dd2a22925a6cebfae31e8261efd53f252cdb1
Smitty Werben <123808974+SmittyWerbenn@users.noreply.github.com>
2026-05-30 09:19:56 +0700

    Update config.js

--- track/config.js
+++ track/config.js
@@ -1,5 +1,5 @@
-
 const API_CONFIG = {
-    BASE_URL: "https://apisanbox.coresyssap.com/v2/shipment/tracking",
-    API_KEY: "DEV_m4rK3tPlac3#_2019" 
+    // Menambahkan proxy di depan URL asli
+    BASE_URL: "https://cors-anywhere.herokuapp.com/https://apisanbox.coresyssap.com/v2/shipment/tracking",
+    API_KEY: "DEV_m4rK3tPlac3#_2019"
 };
